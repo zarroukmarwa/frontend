da2b049179c275a0c5c5e0c7ec9d2f105891f903
marwa zarrouk <zarouk.maroua@gmail.com>
2021-01-19 23:21:48 +0100

    style frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SidebarModule } from './sidebar/sidebar.module';
+
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot,RouterModule, Routes } from '@angular/router'
 import {HttpClientModule} from '@angular/common/http';
 
@@ -93,6 +95,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SidebarModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
     FormsModule,
