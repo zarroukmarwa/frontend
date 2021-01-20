dc596c91e51f2a16f432df2516ccbc0589ddf381
marwa zarrouk <zarouk.maroua@gmail.com>
2021-01-20 01:06:25 +0100

    Update sidebar.component.ts

--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -9,13 +9,10 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
-    { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
-    { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+    { path: '/user', title: 'Client',  icon:'pe-7s-user', class: '' },
+    { path: '/table', title: 'Commande',  icon:'pe-7s-note2', class: '' },
+    { path: '/typography', title: 'Produit',  icon:'pe-7s-news-paper', class: '' },
+    { path: '/icons', title: 'Catégorie',  icon:'pe-7s-science', class: '' },
 ];
 
 @Component({
